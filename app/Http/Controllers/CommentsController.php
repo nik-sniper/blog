40aa71541aab7294f071f45332909953793f2143
Nik_Sniper <nik_mozir@mail.ru>
2019-11-21 19:07:02 +0300

    Добавлены уведомления

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -17,25 +17,18 @@ class CommentsController extends Controller
 {
     public function store(CommentsStoreRequest $request)
     {
-        //dd($request->keys('endpoint'));
         $comment = new Comment;
         $comment->text = $request->get("message");
         $comment->post_id = $request->get("post_id");
         $comment->user_id = Auth::user()->id;
         $comment->save();
-/*
-        $endpoint = $request->endpoint;
-        $token = $request->keys['auth'];
-        $key = $request->keys['p256dh'];*/
+
         $user = Auth::user();
         $user->updatePushSubscription("endpoint", "content");
 
 
-        /*$when = Carbon::now();*/
-
         Notification::send(User::all(), new PushDemo);
-        //$request->user()->notify(new PushDemo());
-        //Notification::send($user, new NewMessage($user));
+
         Notification::send(Auth::user(), new MessageTelegram);
 
         return redirect()->back()->with("status", "Ваш комментарий скоро будет добавлен!");
